2aa7655be8bcc695666f6a4cc90fb462c2c85134
mcpeleeGit <mcpelee@gmail.com>
2020-05-26 00:04:51 +0900

    first setting

---
+++ api/src/test/java/api/dongha/pe/kr/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.dongha.pe.kr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
