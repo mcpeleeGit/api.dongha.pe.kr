16b6a702111a3a40c60716d24dc0ba3208c7570a
mcpeleeGit <mcpelee@gmail.com>
2020-06-04 01:09:14 +0900

    controller

--- src/main/java/api/dongha/pe/kr/example/controller/ExampleController.java
+++ src/main/java/api/dongha/pe/kr/example/controller/ExampleController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class ExampleController {
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello2";
     }
 }
